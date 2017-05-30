0cc242dab8bcef44501ff3030e48619fbe35ec51
Maximilian Antoni <mail@maxantoni.de>
2017-05-30 18:53:55 +0200

    Handle transform error events
    If a transform error occurs, an error message is logged instead of
    throwing up the stack.

---
+++ test/transform-error-test.js
@@ -0,0 +1,58 @@
+/*eslint-env mocha*/
+'use strict';
+
+const assert = require('assert');
+const sinon = require('sinon');
+const Transform = require('stream').Transform;
+const Writable = require('stream').Writable;
+const logger = require('..');
+
+describe('transform error', () => {
+  let sandbox;
+  let log;
+  let out;
+
+  beforeEach(() => {
+    out = '';
+    sandbox = sinon.sandbox.create({ useFakeTimers: true });
+    logger.out(new Writable({
+      write(chunk, enc, done) {
+        out += chunk;
+        done();
+      }
+    }));
+    sandbox.clock.tick(123);
+    log = logger('test');
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+    logger.reset();
+  });
+
+  it('logs error if transform emits "error"', () => {
+    const error = new Error('Ouch!');
+
+    log.ok('Message', { toJSON: () => { throw error; } });
+
+    const entry = JSON.parse(out);
+    assert.equal(entry.ts, 123);
+    assert.equal(entry.ns, 'logger');
+    assert.equal(entry.topic, 'error');
+    assert.equal(entry.msg, 'Transform failed');
+    assert.equal(entry.stack, error.stack);
+  });
+
+  it('handles recursive error', () => {
+    logger.transform(new Transform({
+      transform(chunk, enc, done) {
+        done(new Error('Ouch!'));
+      }
+    }));
+
+    assert.doesNotThrow(() => {
+      log.ok('Message');
+    });
+  });
+
+});
